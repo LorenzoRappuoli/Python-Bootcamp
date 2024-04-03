fad5cf863717fdd0acdb3af942759ad9c705c4a9
LorenzoRappuoli <70167841+LorenzoRappuoli@users.noreply.github.com>
2024-04-03 22:50:39 +0200

    Add files via upload

---
+++ 09. Comprehension/main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+with open("nato_phonetic_alphabet.csv") as content:
+    file = content.readlines()
+
+file = [f.replace("\n","") for f in file]
+
+nato = {n.split(',')[0]: n.split(',')[1] for n in file}
+nato[" "] = " "
+
+testo = input("Write the word to be translated in NATO alphabet: ")
+
+new_word = [nato[a.upper()] for a in testo ]
+new_word = ', '.join(new_word)
+print(new_word)
